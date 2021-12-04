e7c994a0a5c3db0173bd8e1d655d3f73fc397a4f
viafanasyev <voafanasev@edu.hse.ru>
2021-12-04 22:17:35 +0300

    [04] Add solution

---
+++ 04/01/Main.hs
@@ -0,0 +1,63 @@
+module Main where
+
+import           Data.List          (find)
+import           Data.List.Split    (splitOn, splitWhen)
+import           System.Environment
+import           Text.Printf
+
+sumOn :: (Num b) => (a -> b) -> [a] -> b
+sumOn f = foldl (\acc x -> acc + f x) 0
+
+transpose :: [[a]] -> [[a]]
+transpose ([]:_) = []
+transpose x      = (map head x) : transpose (map tail x)
+
+type Board = [[(Int, Bool)]]
+
+sumBoard :: Board -> Int
+sumBoard = sum . map (sumOn fst) . map (filter (not . snd))
+
+mark :: Int -> Board -> Board
+mark _ [] = []
+mark x (row:rows) = (markRow row) : mark x rows
+  where
+    markRow :: [(Int, Bool)] -> [(Int, Bool)]
+    markRow []                   = []
+    markRow ((elem, flag):elems) = (elem, flag || elem == x) : markRow elems
+
+bingo :: Board -> Bool
+bingo b = bingoRows b || bingoColumns b
+  where
+    bingoRows :: Board -> Bool
+    bingoRows = any $ all snd
+    bingoColumns :: Board -> Bool
+    bingoColumns = bingoRows . transpose
+
+playGame :: [Board] -> [Int] -> (Int, Board)
+playGame _ [] = undefined
+playGame boards (x:xs) =
+  case maybeBingoBoard of
+    Nothing         -> playGame newBoards xs
+    Just bingoBoard -> (x, bingoBoard)
+  where
+    newBoards = map (mark x) boards
+    maybeBingoBoard = find bingo newBoards
+
+readBoard :: [String] -> Board
+readBoard [] = []
+readBoard (row:rows) = (map (\x -> (read x, False)) $ words row) : readBoard rows
+
+main :: IO ()
+main = do
+  args <- getArgs
+  case args of
+    (filePath:_) -> do
+      printf "Input file: %s\n" filePath
+      rows <- lines <$> readFile filePath
+      let values = map read $ splitOn "," $ head rows :: [Int]
+      let boards = map readBoard $ splitWhen null $ drop 2 rows
+      let (winningVal, winningBoard) = playGame boards values
+      let winningBoardSum = sumBoard winningBoard
+      printf "Winning value: %d, winning board sum: %d\n" winningVal winningBoardSum
+      printf "Ans: %d\n" $ winningVal * winningBoardSum
+    _ -> error "Expected input file"
